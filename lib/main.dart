d439f0d4055121c28f0d7d43b6ece2bf7116f627
jmyoo55 <jmyoo88@naver.com>
2021-04-02 01:18:23 +0900

    Add Schedule check popup #2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:autocalen/models/schedule.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -43,6 +44,16 @@ class _CalendarPageState extends State<CalendarPage> {
   String _monthName ='';
   String _yearName='';
 
+  // 선택한 날짜에 등록된 일정 있는지
+  bool isEmpty = true;
+  String _dateText ='';
+
+  @override
+  void initState(){
+    _dateText = '';
+    super.initState();
+  }
+
   void changeAppBarDateView(ViewChangedDetails viewChangedDetails){
     //앱바에 년월 바꾸기
     SchedulerBinding.instance
@@ -60,6 +71,109 @@ class _CalendarPageState extends State<CalendarPage> {
     changeAppBarDateView(viewChangedDetails);
     //showTheImage();
   }
+
+  ScheduleDataSource getCalendarDataSource() {
+    // 스케줄 데이터 추가
+    List<Schedule> schedules = <Schedule>[];
+
+    final DateTime today = DateTime.now(); // 오늘 날짜
+    final DateTime startTime = DateTime(today.year, today.month, today.day, 9, 0, 0); // 시작 날짜 (년, 월, 일, 시, 분, 초)
+    final DateTime startTime2 = DateTime(today.year, today.month, today.day); // 시작 날짜 (년, 월, 일, 시, 분, 초)
+    final DateTime endTime = startTime.add(const Duration(hours: 2)); // 종료 날짜
+    final DateTime endTime2 = startTime2.add(const Duration(days: 3)).subtract(const Duration(seconds: 1)); // 3일동안
+    final DateTime endTime3 = DateTime(today.year, today.month, today.day+1);
+
+    schedules.add(Schedule(
+        'Conference', startTime, endTime, const Color(0xFF0F8644), false));
+    schedules.add(Schedule(
+        'Meeting', startTime2, endTime2, const Color(0xFFFF32e2), false));
+    schedules.add(Schedule(
+        'Study', startTime, endTime3, const Color(0xFF0032e2), false));
+    schedules.add(Schedule(
+        'Play', startTime2, endTime, const Color(0xFFFF0000), false));
+    schedules.add(Schedule(
+        'Algorithm Study', startTime2, endTime2, const Color(0xFFFFFF00), false));
+    return ScheduleDataSource(schedules);
+  }
+
+  void calendarTapped(CalendarTapDetails details) {
+    // 캘린더 날짜 탭한 경우
+    if (details.targetElement == CalendarElement.calendarCell) {
+      _dateText = DateFormat('yyyy년 MM월 dd일 (E)', 'ko').format(details.date).toString();
+
+      // 선택한 날짜에 일정이 있는 경우
+      if (details.appointments.length > 0) isEmpty = false;
+      // 선택한 날짜에 일정이 없는 경우
+      else isEmpty = true;
+
+      showDialog( // 팝업창 띄우기
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Container(
+                  padding:EdgeInsets.symmetric(horizontal: 10.0, vertical: 0.0),
+                  child: new Text('$_dateText')
+              ),
+              content:Container(
+                height: MediaQuery.of(context).size.height*0.45,
+                width: MediaQuery.of(context).size.width*0.8,
+                child: _buildScheduleListView(context, details),
+              ),
+              shape: RoundedRectangleBorder( // 모서리 둥글게
+                  borderRadius: BorderRadius.all(Radius.circular(10.0))),
+              actions: <Widget>[
+              ],
+            );
+          });
+    }
+  }
+
+  String subtitleDate(DateTime startTime, DateTime endTime){
+    String subtitle;
+
+    // 시작날짜, 종료 날짜 비교
+    if(startTime.difference(endTime).inDays==0){ // 시작날짜, 종료날짜가 같으면
+      subtitle= DateFormat('MM월 dd일').format(startTime).toString();
+    } else{
+      subtitle='${DateFormat('MM월 dd일').format(startTime).toString()} - ${DateFormat('MM월 dd일').format(endTime).toString()}';
+    }
+    return subtitle;
+  }
+
+  // 날짜 클릭시 뜨는 팝업창 내부 리스트 뷰 (해당 날짜에 저장된 일정 리스트로 뜨도록..)
+  Widget _buildScheduleListView(BuildContext context, CalendarTapDetails details){
+    if(isEmpty){ // 일정 없는 경우
+      return Container(
+          padding: EdgeInsets.symmetric(horizontal:10.0, vertical: 5.0),
+          child: Text('등록된 일정이 없습니다'));
+    }
+    else{
+      return ListView.separated(
+        itemCount: details.appointments.length,
+        itemBuilder: (BuildContext _context, int i){
+          return ListTile(
+            minLeadingWidth: 10, // leading 부분 최소 넓이
+            leading: SizedBox(
+              width: 5,
+              child: Container(
+                  color: details.appointments[i].tag
+              ),
+            ),
+            minVerticalPadding: 4.0, // 각 일정 탭 별 padding 값
+            contentPadding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 0.0),
+            title: Text(details.appointments[i].title),
+            subtitle: Text(subtitleDate(details.appointments[i].startTime, details.appointments[i].endTime)),
+            onTap: () {// 일정 탭한 경우
+            },
+          );
+        },
+        separatorBuilder: (context, index) { return Divider(
+          height: 0.0, // 구분선 높이 조정
+        );},
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -112,6 +226,8 @@ class _CalendarPageState extends State<CalendarPage> {
             selectionDecoration: BoxDecoration(
               color: Colors.transparent
             ),
+            dataSource: getCalendarDataSource(),
+            onTap: calendarTapped
           ),
         ),
         floatingActionButton: AddImgFAB()
