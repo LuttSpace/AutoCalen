9610d64768634b399a49f9d3af5fe1ee479badfe
Kim Yeri <98sena@naver.com>
2021-04-09 01:38:21 +0900

    Change DayDialogWidget UI & Add FAB #19

--- lib/widgets/DayDialogWidget.dart
+++ lib/widgets/DayDialogWidget.dart
@@ -1,3 +1,4 @@
+import 'package:autocalen/widgets/DayDialogFABWidget.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
@@ -18,7 +19,6 @@ class ShowDayDialog extends StatefulWidget{
 class _ShowDayDialogState extends State<ShowDayDialog> {
   String subtitleDate(DateTime startTime, DateTime endTime){
     String subtitle;
-
     // 시작날짜, 종료 날짜 비교
     if(startTime.difference(endTime).inDays==0){ // 시작날짜, 종료날짜가 같으면
       subtitle= DateFormat('MM월 dd일').format(startTime).toString();
@@ -30,10 +30,53 @@ class _ShowDayDialogState extends State<ShowDayDialog> {
 
 // 날짜 클릭시 뜨는 팝업창 내부 리스트 뷰 (해당 날짜에 저장된 일정 리스트로 뜨도록..)
   Widget _buildScheduleListView(BuildContext context, CalendarTapDetails details){
+    // return Scaffold(
+    //   body: widget.isEmpty?
+    //       Container(
+    //         padding: EdgeInsets.symmetric(horizontal:10.0, vertical: 5.0),
+    //         child: Text('등록된 일정이 없습니다')) :
+    //   ListView.separated(
+    //     itemCount: details.appointments.length,
+    //     itemBuilder: (BuildContext _context, int i){
+    //       return ListTile(
+    //         minLeadingWidth: 10, // leading 부분 최소 넓이
+    //         leading: SizedBox(
+    //           width: 5,
+    //           child: Container(
+    //               color: details.appointments[i].tag
+    //           ),
+    //         ),
+    //         minVerticalPadding: 4.0, // 각 일정 탭 별 padding 값
+    //         contentPadding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 0.0),
+    //         title: Text(details.appointments[i].title),
+    //         subtitle: Text(subtitleDate(details.appointments[i].startTime, details.appointments[i].endTime)),
+    //         onTap: () {// 일정 탭한 경우
+    //           showBarModalBottomSheet(
+    //             context: context,
+    //             builder:(context)=> ScheduleInputModal(),
+    //             expand: true,
+    //           );
+    //           // showDialog(
+    //           //     context: context,
+    //           //     builder: (BuildContext context) {
+    //           //       return MaterialApp(
+    //           //         home: ScheduleInputForm(),
+    //           //       );
+    //           //     }
+    //           // );
+    //         },
+    //       );
+    //     },
+    //     separatorBuilder: (context, index) { return Divider(
+    //       height: 0.0, // 구분선 높이 조정
+    //     );},
+    //   ),
+    //);
+
     if(widget.isEmpty){ // 일정 없는 경우
       return Container(
-          padding: EdgeInsets.symmetric(horizontal:10.0, vertical: 5.0),
-          child: Text('등록된 일정이 없습니다'));
+          padding: EdgeInsets.symmetric(horizontal:10.0, vertical: 15.0),
+          child: Text('등록된 일정이 없습니다'),);
     }
     else{
       return ListView.separated(
@@ -42,7 +85,7 @@ class _ShowDayDialogState extends State<ShowDayDialog> {
           return ListTile(
             minLeadingWidth: 10, // leading 부분 최소 넓이
             leading: SizedBox(
-              width: 5,
+              width: 8,
               child: Container(
                   color: details.appointments[i].tag
               ),
@@ -77,20 +120,65 @@ class _ShowDayDialogState extends State<ShowDayDialog> {
 
   @override
   Widget build(BuildContext context) {
-    return AlertDialog(
-            title: Container(
-                padding:EdgeInsets.symmetric(horizontal: 10.0, vertical: 0.0),
-                child: new Text('${widget._dateText}')
-            ),
-            content:Container(
-              height: MediaQuery.of(context).size.height*0.45,
-              width: MediaQuery.of(context).size.width*0.8,
-              child: _buildScheduleListView(context, widget.details),
-            ),
-            shape: RoundedRectangleBorder( // 모서리 둥글게
-                borderRadius: BorderRadius.all(Radius.circular(10.0))),
-            actions: <Widget>[
-            ],
-          );
+    return Dialog(
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20.0)
+      ),
+      child: Container(
+        height: MediaQuery.of(context).size.height*0.65,
+        child: Stack(
+          children: [
+              Positioned(
+                  child: Container(
+                      alignment: Alignment.topCenter,
+                      padding:EdgeInsets.symmetric(horizontal: 0.0, vertical: 15.0),
+                      child: new Text(
+                        '${widget._dateText}',
+                        style: TextStyle(
+                            fontSize: 22
+                        ),
+                      )
+                  ),
+              ),
+              Positioned(
+                  top: 40,
+                  child: Container(
+                    height: MediaQuery.of(context).size.height*1.8,
+                    width: MediaQuery.of(context).size.width*0.9,
+                    child: _buildScheduleListView(context, widget.details),
+                    padding: EdgeInsets.symmetric(horizontal: 10,vertical: 0),
+                  ),
+              ),
+              Positioned(
+                  bottom: 10,
+                  right: 10,
+                  child: Container(
+                    height: MediaQuery.of(context).size.height*0.45,
+                    width: MediaQuery.of(context).size.width*0.9,
+                    alignment: Alignment.bottomRight,
+                    child: DayDialogFAB(),
+                  )
+              ),
+          ],
+        ),
+      ),
+    );
+
+
+      // AlertDialog(
+      //       title: Container(
+      //           padding:EdgeInsets.symmetric(horizontal: 10.0, vertical: 0.0),
+      //           child: new Text('${widget._dateText}')
+      //       ),
+      //       content:Container(
+      //         height: MediaQuery.of(context).size.height*0.45,
+      //         width: MediaQuery.of(context).size.width*0.8,
+      //         child: _buildScheduleListView(context, widget.details),
+      //       ),
+      //       shape: RoundedRectangleBorder( // 모서리 둥글게
+      //           borderRadius: BorderRadius.all(Radius.circular(10.0))),
+      //       actions: <Widget>[
+      //       ],
+      //     );
   }
 }
