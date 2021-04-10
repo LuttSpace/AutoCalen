2afd9ff27502d3aa140c87c9ec96c43a241a0bc6
jmyoo55 <jmyoo88@naver.com>
2021-04-10 17:22:21 +0900

    Add Google Login #16

--- lib/widgets/DrawerWidget.dart
+++ lib/widgets/DrawerWidget.dart
@@ -1,4 +1,5 @@
 import 'package:autocalen/pages/TagSettingPage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ShowDrawer extends StatefulWidget{
@@ -7,41 +8,58 @@ class ShowDrawer extends StatefulWidget{
 }
 
 class _ShowDrawerState extends State<ShowDrawer> {
+  FirebaseAuth auth = FirebaseAuth.instance;
+
+  signOut() async {
+    await auth.signOut();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-        child: ListView(
-          children: [
-            UserAccountsDrawerHeader(
-              arrowColor: Colors.pink,
-              accountEmail: Text('98sena@naver.com'),
-              accountName: Text('김예리'),
-              currentAccountPicture: CircleAvatar(
-                child: Icon(Icons.person,color: Colors.black),
-                backgroundColor: Colors.white,
+    if (auth.currentUser != null) {
+      return Drawer(
+          child: ListView(
+            children: [
+              UserAccountsDrawerHeader(
+                arrowColor: Colors.pink,
+                accountEmail: Text('${auth.currentUser.email}'),
+                accountName: Text('${auth.currentUser.displayName}'),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage(auth.currentUser.photoURL),
+                ),
+                decoration: BoxDecoration(
+                    color: Colors.black12
+                ),
               ),
-              decoration: BoxDecoration(
-                  color: Colors.black12
+              ListTile(
+                leading: Icon(Icons.color_lens),
+                title: Text('태그 설정'),
+                onTap: () {
+                  print('태그 설정');
+                  //Navigator.push(context, MaterialPageRoute(builder: (context)=> TagSetting()));
+                  Navigator.pushNamed(context, '/tagSetting');
+                },
               ),
-            ),
-            ListTile(
-              leading: Icon(Icons.color_lens),
-              title: Text('태그 설정'),
-              onTap: (){
-                print('태그 설정');
-                //Navigator.push(context, MaterialPageRoute(builder: (context)=> TagSetting()));
-                Navigator.pushNamed(context, '/tagSetting');
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.settings),
-              title: Text('설정'),
-              onTap: (){
-                print('설정');
-              },
-            ),
-          ],
-        )
-    );
+              ListTile(
+                leading: Icon(Icons.settings),
+                title: Text('설정'),
+                onTap: () {
+                  print('설정');
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.logout),
+                title: Text('로그아웃'),
+                onTap: (){
+                  print('로그아웃');
+                  Navigator.pop(context);
+                  signOut();
+                },
+              ),
+            ],
+          )
+      );
+    }
+    return Drawer();
   }
 }
