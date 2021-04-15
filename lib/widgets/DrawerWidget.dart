eb80100327747f17b0a4577eb86ac1537c5afde4
jmyoo55 <jmyoo88@naver.com>
2021-04-15 16:47:30 +0900

    Add Provider (User data) #29

--- lib/widgets/DrawerWidget.dart
+++ lib/widgets/DrawerWidget.dart
@@ -1,6 +1,8 @@
+import 'package:autocalen/models/UserData.dart';
 import 'package:autocalen/pages/TagSettingPage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ShowDrawer extends StatefulWidget{
   @override
@@ -16,52 +18,51 @@ class _ShowDrawerState extends State<ShowDrawer> {
 
   @override
   Widget build(BuildContext context) {
-    if (auth.currentUser != null) {
-      return Drawer(
-          child: ListView(
-            children: [
-              UserAccountsDrawerHeader(
-                arrowColor: Colors.pink,
-                accountEmail: Text('${auth.currentUser.email}'),
-                accountName: Text('${auth.currentUser.displayName}'),
-                currentAccountPicture: CircleAvatar(
-                  child: auth.currentUser.photoURL==null ? Icon(Icons.person,color: Colors.black): null,
-                  backgroundImage: auth.currentUser.photoURL!=null? NetworkImage(auth.currentUser.photoURL):null,
-                  backgroundColor: Colors.white,
-                ),
-                decoration: BoxDecoration(
-                    color: Colors.black12
-                ),
+    final userProvider = Provider.of<UserData>(context, listen: false);
+    return Drawer(
+        child: userProvider.getUid() != null? ListView(
+          children: [
+            UserAccountsDrawerHeader(
+              arrowColor: Colors.pink,
+              accountEmail: Text('${userProvider.getEmail()}'),
+              accountName: Text('${userProvider.getName()}'),
+              currentAccountPicture: CircleAvatar(
+                child: userProvider.getPhotoURL()==null ? Icon(Icons.person,color: Colors.black): null,
+                backgroundImage: userProvider.getPhotoURL()!=null? NetworkImage(userProvider.getPhotoURL()):null,
+                backgroundColor: Colors.white,
               ),
-              ListTile(
-                leading: Icon(Icons.color_lens),
-                title: Text('태그 설정'),
-                onTap: () {
-                  print('태그 설정');
-                  //Navigator.push(context, MaterialPageRoute(builder: (context)=> TagSetting()));
-                  Navigator.pushNamed(context, '/tagSetting');
-                },
+              decoration: BoxDecoration(
+                  color: Colors.black12
               ),
-              ListTile(
-                leading: Icon(Icons.settings),
-                title: Text('설정'),
-                onTap: () {
-                  print('설정');
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.logout),
-                title: Text('로그아웃'),
-                onTap: (){
-                  print('로그아웃');
-                  Navigator.pop(context);
-                  signOut();
-                },
-              ),
-            ],
-          )
-      );
-    }
-    return Drawer();
+            ),
+            ListTile(
+              leading: Icon(Icons.color_lens),
+              title: Text('태그 설정'),
+              onTap: () {
+                print('태그 설정');
+                //Navigator.push(context, MaterialPageRoute(builder: (context)=> TagSetting()));
+                Navigator.pushNamed(context, '/tagSetting');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('설정'),
+              onTap: () {
+                print('설정');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.logout),
+              title: Text('로그아웃'),
+              onTap: (){
+                print('로그아웃');
+                Navigator.pop(context);
+                signOut();
+                userProvider.resetUserData(); // Provider 사용자 정보 초기화
+              },
+            ),
+          ],
+        ): null,
+    );
   }
 }
